da148146243f48b6210a33c4a62db5c5aa6def91
rokss-alt <65897674+rokss-alt@users.noreply.github.com>
2021-10-16 20:22:37 +0300

    Add files via upload

---
+++ spam_bot.py
@@ -0,0 +1,16 @@
+
+
+import pyautogui
+import time
+
+
+spam_text = input("text: ")
+times = int(input("How many times: "))
+time.sleep(10)
+for _ in range (times):
+	pyautogui.typewrite(spam_text)
+	pyautogui.press('enter')
+	time.sleep(1)
+	times = times - 1
+	print(times)
+
